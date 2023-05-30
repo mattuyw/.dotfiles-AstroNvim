67cb757fd0b1a5ecf84297f429361691bda665aa
Matthew Nichols <matthew.nichols@gc.com>
2023-05-29 23:11:52 -0500

    add custom filetypes

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
 return {
-  colorscheme = "catppuccin"
+  colorscheme = "catppuccin",
+
+  -- This function is run last and is a good place to configuring
+  -- augroups/autocommands and custom filetypes also this just pure lua so
+  -- anything that doesn't fit in the normal config locations above can go here
+  polish = function()
+    -- Set up custom filetypes
+    vim.filetype.add {
+      pattern = {
+        ["Makefile.*"] = "make",
+        [".Makefile.*"] = "make",
+        ["Dockerfile.*"] = "dockerfile",
+      },
+    }
+  end,
 }
